41461168e8438d4549ed7abfba289083a8c9284d
Josh Baugh <joshbaughmusic@gmail.com>
2023-09-18 10:28:12 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -34,7 +34,11 @@ int minAwesomeness = 0;
 int maxAwesomeness = 100;
 
 // Make a new "Adventurer" object using the "Adventurer" class
-Adventurer theAdventurer = new Adventurer("Jack");
+Console.WriteLine($"Enter your name to begin your adventure:");
+
+string adventurerNameInput = Console.ReadLine();
+
+Adventurer theAdventurer = new Adventurer(adventurerNameInput);
 
 // A list of challenges for the Adventurer to complete
 // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
